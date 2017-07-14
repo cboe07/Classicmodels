b40286d448b1035f17ff970f346b6dfa22c0fffd
Chris Boeckel <cboe07@gmail.com>
2017-07-14 13:23:54 -0400

    Add error check, email auth, navbar update

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-// Include config file. Go up from routes, down into config
+// Include config file. Go up from routes, down into config, config.js
 var config = require('../config/config');
 
-// include b-crypt for hashing password
+// include bcrpyt for hasing and checking password
 var bcrypt = require('bcrypt-nodejs');
-// include rand token for generating user token
-var randToken = require('rand-token');
+// include rand-token for generating user token
+var randToken = require('rand-token')
 
-// setup the connection with options
+// set up the connection with options
 var connection = mysql.createConnection({
 	host: config.host,
 	user: config.user,
@@ -19,86 +19,91 @@ var connection = mysql.createConnection({
 // Actually make the connection
 connection.connect();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/:something/:something2',(req,res, next)=>{
+// 	req.cookies.url = req.body.param.something
+// 	next()
+// })
 
-router.get('/productlines/get', (req,res)=>{
+router.get('/productlines/get', (req, res)=>{
 	const selectQuery = "SELECT * FROM productlines"
-	connection.query(selectQuery,(error,results,fields)=>{
+	connection.query(selectQuery,(error, results, fields)=>{
 		if(error){
-			res.json(error);
+			res.json(error)
 		}else{
 			res.json(results);
 		}
-	})
-})
+	});
+});
 
-router.post('/register', (req,res)=>{
+router.post('/register', (req, res)=>{
+	console.log(req.body)
+
 	const name = req.body.name;
 	const email = req.body.email;
 	const accountType = 'customer';
-	const username = req.body.username
 	const password = bcrypt.hashSync(req.body.password);
-	// console.log(req.body)
 	const city = req.body.city;
 	const state = req.body.state;
 	const salesRep = req.body.salesRep
 	const creditLimit = 16000000
 
-
-
-
-	
-
-
 	// We want to insert the user into 2 tables: Customers and Users.
-	// Users needs the customerNumber from the customers table.
+	// Users needs the customerNumber from the Customers table.
 	// Therefore, we need to insert the user into Customers first...
-	// get the ID created by that insert, THEN insert the user into USers...
-	var checkUsername = "SELECT * FROM users WHERE username = ?"
-	connection.query(checkUsername, [username],(error,results)=>{
-		if(results.length == 1){
-			// Customers insert query
-			var insertIntoCust = "INSERT INTO customers (customerName,city,state,salesRepEmployeeNumber,creditLimit) VALUES (?,?,?,?,?)"
-			// Run the query (for now autoset the sales re to 1337)
-			connection.query(insertIntoCust, [name,city,state,1337,creditLimit],(error,results,fields)=>{
-				// console.log(error)
-				// console.log(results)
+	// get the ID created by that insert, THEN insert the user into Users.
+
+// First, check to see if email already exists
+	const checkEmail = new Promise((resolve, reject) => {
+		const checkEmailQuery = "SELECT * FROM users WHERE email = ?";
+		connection.query(checkEmailQuery,[email],(error,results)=>{
+			if(error) throw error;
+			if(results.length > 0){
+				reject({msg: "userAlreadyExists"});
+			}else{
+				// we dont care about results. Just that there isn't a match
+				resolve();
+			}
+		})
+	})
+
+	checkEmail.then(
+		// Customers insert query
+		()=>{
+			var insertIntoCust = "INSERT INTO customers (customerName, city, state, salesRepEmployeeNumber, creditLimit) VALUES (?,?,?,?,?)"
+			// Run the query (for now autoset the sales rep to 1337)
+			connection.query(insertIntoCust,[name,city,state,1337,creditLimit],(error, results)=>{
 				// Get the ID that was used in the customers insert
-				const newId = results.insertId
+				const newID = results.insertId
 				// Get the current timestamp
-				var currTimeStamp = parseInt(Date.now() / 1000)
+				var currTimeStamp = parseInt(Date.now() / 1000);
 				// Set up a token for this user. We will give this back to React
 				var token = randToken.uid(40);
 				// Users insert query
-				var insertQuery = "INSERT INTO users (uid,type,username,password,created,token) VALUES (?,?,?,?,?,?)"
-				// Run query. Use error2 andresults2 bc error and results used already
+				var insertQuery = "INSERT INTO users (uid,type,password,created,token,email) VALUES (?,?,?,?,?,?)";
+				// Run the query. Use error2 and results2 because are already used results and error
 				
-				connection.query(insertQuery,[newId,accountType,username,password,currTimeStamp,token],(error2,results2)=>{
-					if(error){
+				connection.query(insertQuery,[newID, accountType,password, currTimeStamp, token, email],(error2,results2)=>{
+					if(error2){
 						res.json({
 							msg: error2
 						})
 					}else{
 						res.json({
 							msg: "userInserted",
-							token: token
-						})
+							token: token,
+							name: name
+						});
 					}
-				})
-				
-			})
-		}else{
-			console.log("UserAlreadyExits")
-			res.json({
-				msg: 'userAlreadyExists'
+				});
 			})
 		}
-	})
-})
+	).catch(
+		(error)=>{
+			res.json(error)
+		}
+	)
 
+})	
 
 module.exports = router;
 
--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -1,140 +1,156 @@
 import React, {Component} from 'react';
-import { Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap';
-import { bindActionCreators } from 'redux';
-// Get the registerAction function whoch runs on submission
+import { Form, FormGroup, ControlLabel, FormControl, Button, Col ,MenuItem} from 'react-bootstrap'
+// Our action needs bindActionCreators from redux
+import  {bindActionCreators} from 'redux';
+// Get the registerAction function which runs on submission
 import RegisterAction from '../actions/RegisterAction';
-// Import connect bc this is a. container
+// Because this is a container, we need connect from react-redux!
 import {connect} from 'react-redux';
+
 
 class Register extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
-			registerMessage: ""
+			registerMessage: "",
+			nameError: null,
+			emailError: null,
+			formError: false
 		}
 		this.handleRegistration = this.handleRegistration.bind(this);
 	}
 
 	handleRegistration(event){
 		event.preventDefault();
-		// console.log("User submitted the form")
+		// console.log("User SUbmitted the form!!")
 		var name = event.target[0].value
-		var email= event.target[1].value
-		var accountType = 'customer'
-		var username = event.target[3].value
-		var password = event.target[4].value
-		var city = event.target[5].value
-		var state = event.target[6].value
-		var salesRep = event.target[7].value
-		// console.log(name);
-		this.props.registerAction({
-			name: name,
-			email: email,
-			accountType: accountType,
-			username: username,
-			password: password,
-			city: city,
-			state: state,
-			salesRep: salesRep
+		var email = event.target[1].value
+		var accountType = "customer"
+		var password = event.target[3].value
+		var city = event.target[4].value
+		var state = event.target[5].value
+		var salesRep = event.target[6].value
+		var error = false;
 
-		});
-	
+		//Name
+		if(name.length < 3){
+			var nameError = "error"; 
+			error=true;
+		}
+		else{ 
+			var nameError = "success"
+		}
+
+		//Email
+		if(email.length < 3){var emailError = "error"; error=true}
+		else{var emailError = "success"}
+
+
+		// console.log(name);
+		if(error){
+			this.setState({
+				formError: true,
+				emailError: emailError,
+				nameError: nameError
+			}) 
+		}else{
+			this.props.registerAction({
+				name: name,
+				email: email,
+				accountType: accountType,
+				password: password,
+				city: city,
+				state: state,
+				salesRep: salesRep
+			});
+		}
 	}
 
+	componentWillReceiveProps(nextProps) {
+		console.log("=======================")
+		console.log(nextProps.registerResponse)
+		console.log("=======================")
 
-	componentWillUpdate(nextProps, nextState) {
-		if(this.props.registerResponse.msg == 'userInserted'){
+		if(nextProps.registerResponse.msg == 'userInserted'){
 			this.props.history.push('/');
-		}else if(this.props.registerResponse.msg == 'userAlreadyExists')
-			this.setState.registerMessage = 'User already Exists'
+		}else if(nextProps.registerResponse.msg == 'userAlreadyExists'){
+			console.log("User name taken!")
+			this.setState({
+				registerMessage: "This email is already linked to an account."
+			})
+		}		
 	}
 
 	render(){
-		console.log("=============================")
-		console.log(this.props.registerResponse)
-		console.log("=============================")
-
-		
 
 		return(
-			
-			<div className='container main register-wrapper'>
+			<div className="register-wrapper">
 				<h1>{this.state.registerMessage}</h1>
 				<Form horizontal onSubmit={this.handleRegistration}>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName" validationState={this.state.nameError}>
 						<Col componentClass={ControlLabel} sm={2}>
 							Name
 						</Col>
 						<Col sm={6}>
-							<FormControl type='text' placeholder='Full Name' />
+							<FormControl type="text" name="fullName" placeholder="Full Name" />
 						</Col>
 					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName" validationState={this.state.emailError}>
 						<Col componentClass={ControlLabel} sm={2}>
 							Email
 						</Col>
 						<Col sm={6}>
-							<FormControl type='text' name='email' placeholder='Email' />
+							<FormControl type="email" name="email" placeholder="Email" />
 						</Col>
 					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName">
 						<Col componentClass={ControlLabel} sm={2}>
 							Account Type
 						</Col>
 						<Col sm={6}>
-							<FormControl type='text' name='type' value='Customer' disabled/>
+							<FormControl type="text" name="type" value="customer" disabled />
 						</Col>
 					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
-						<Col componentClass={ControlLabel} sm={2}>
-							Username
-						</Col>
-						<Col sm={6}>
-							<FormControl type='text' name='username' placeholder='Username' />
-						</Col>
-					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName">
 						<Col componentClass={ControlLabel} sm={2}>
 							Password
 						</Col>
 						<Col sm={6}>
-							<FormControl type='password' name='password' placeholder='Password' />
+							<FormControl type="password" name="password" placeholder="Password" />
 						</Col>
 					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName">
 						<Col componentClass={ControlLabel} sm={2}>
 							City
 						</Col>
 						<Col sm={6}>
-							<FormControl type='text' name='city' placeholder='City' />
+							<FormControl type="text" name="city" placeholder="City" />
 						</Col>
 					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName">
 						<Col componentClass={ControlLabel} sm={2}>
 							State
 						</Col>
 						<Col sm={6}>
-							<FormControl type='text' name='state' placeholder='State' />
+							<FormControl type="text" name="state" placeholder="State" />
 						</Col>
 					</FormGroup>
-					<FormGroup controlId='formHorizontalName'>
+					<FormGroup controlId="formHorizontalName">
 						<Col componentClass={ControlLabel} sm={2}>
-							Sales Representative
+							Sales Rep
 						</Col>
 						<Col sm={6}>
-							<FormControl type='text' name='employee' placeholder='Employee worked with' />
+							<FormControl type="text" name="employee" placeholder="Employee you worked with" />
 						</Col>
 					</FormGroup>
 					<FormGroup>
 						<Col smOffset={2} sm={6}>
-							<Button bsStyle='primary' bsSize='small' type='submit'>
+							<Button bsStyle="primary" bsSize="small" type="submit">
 								Register
 							</Button>
 						</Col>
 					</FormGroup>
 				</Form>
-
-				
 			</div>
 		)
 	}
@@ -148,10 +164,9 @@ function mapStateToProps(state){
 
 function mapDispatchToProps(dispatch){
 	return bindActionCreators({
-		registerAction: RegisterAction 
-	},dispatch)
+		registerAction: RegisterAction
+	}, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Register);
-
-
+// export default Register;
+export default connect(mapStateToProps,mapDispatchToProps)(Register);
